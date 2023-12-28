9e2671bead3fbffeba6e711f29f917e08e2b123b
wenderson guedes <wendersonguedes6@gmail.com>
2023-12-28 11:33:34 -0300

    other

--- basico/tipos/index.dart
+++ basico/tipos/index.dart
@@ -30,17 +30,26 @@ void main() {
   // String frase2 = ' ${num1 * 2} vezes a mais braba';
   // print(frase + frase2); // He4art devs é a mais braba
 
-  int day = 25;
-  int year = 2023;
-  int month = 12;
-  int num1 = 19;
-  int num2 = 10;
+  // int day = 25;
+  // int year = 2023;
+  // int month = 12;
+  // int num1 = 19;
+  // int num2 = 10;
 
-  String name = 'wend';
+  // String name = 'wend';
 
-  String phrase = 'meu nome é $name e escrevi isso dia $day/$month/$year';
-  String otherPhrase = 'o resultado da soma de $num1 e $num2 é: ${num1 + num2}';
+  // String phrase = 'meu nome é $name e escrevi isso dia $day/$month/$year';
+  // String otherPhrase = 'o resultado da soma de $num1 e $num2 é: ${num1 + num2}';
 
-  print(phrase);
-  print(otherPhrase);
+  // print(phrase);
+  // print(otherPhrase);
+
+  // num x = 1;
+  // x = x + 2.5;
+  // print(x);
+
+  // var usersName = ['wend', 'bruno', 'guilherme'];
+  // print(usersName);
+
+  List<int> randomNumbers = [1, 2, 3];
 }
