69c4d2974464eb1b9ebe406b2b4cdfdc9d34aa4e
wenderson guedes <wendersonguedes6@gmail.com>
2024-01-10 00:28:52 -0300

    arquivos dart auxiliares

---
+++ fundamentos/01-tipos/index.dart
@@ -0,0 +1,59 @@
+void main() {
+  // bool valorLogico = true;
+  // double valorDecimal = 10.1;
+
+  // print(valorLogico + valorDecimal);
+
+  // double valorDob = 23.6;
+  // int valorInt = valorDob.toInt();
+
+  // print(valorInt);
+
+  // String firstName = 'wend';
+  // String lastName = ' guedes';
+
+  // String fullName = firstName + lastName;
+
+  // print(fullName);
+
+  // bool isAdmin = true;
+  // bool hasPermission = false;
+
+  // if (isAdmin != true) {
+  //   print('oi');
+  // } else {
+  //   print('usuario comum');
+  // }
+
+  // int num1 = 5;
+  // String frase = 'He4art devs é';
+  // String frase2 = ' ${num1 * 2} vezes a mais braba';
+  // print(frase + frase2); // He4art devs é a mais braba
+
+  // int day = 25;
+  // int year = 2023;
+  // int month = 12;
+  // int num1 = 19;
+  // int num2 = 10;
+
+  // String name = 'wend';
+
+  // String phrase = 'meu nome é $name e escrevi isso dia $day/$month/$year';
+  // String otherPhrase = 'o resultado da soma de $num1 e $num2 é: ${num1 + num2}';
+
+  // print(phrase);
+  // print(otherPhrase);
+
+  // num x = 1;
+  // x = x + 2.5;
+  // print(x);
+
+  // var usersName = ['wend', 'bruno', 'guilherme'];
+  // print(usersName);
+
+  // List<int> randomNumbers = [1, 2, 3];
+  // print(randomNumbers);
+
+  // var names = <String>{'wend', 'luara', 'luanna'};
+  // print(names);
+}
---
+++ fundamentos/04-estruturas-condicionais/index.dart
@@ -0,0 +1,9 @@
+void main() {
+  const year = 2022;
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+}
